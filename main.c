d257bce1b4da11b34a68b84d7029a754bdeaf5b6
dumbyplug <iwasdumbyforu@gmail.com>
2025-03-19 16:12:29 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "create_show.c"
 #include "random_advice.c"
+#include <time.h>
 
 char game(char player){
+	srand(time(NULL));
 	char initialSymbol = player;
 	char grid[6][7];
 	createGrid(grid);
@@ -9,7 +11,7 @@ char game(char player){
 	char run = 1;
 	int move;
 	while(run){
-		//printf("\x1b[H\x1b[2J");
+		printf("\x1b[H\x1b[2J");
 		showGrid(grid);
 
 		printf("You can play on %d\n", advanced_advice(grid, player) + 1);
@@ -20,6 +22,7 @@ char game(char player){
 			drop_disk(grid, move - 1, player);
 
 			if(alignment(grid, player) >= 4){
+				printf("\x1b[H\x1b[2J");
 				showGrid(grid);
 				printf(" -- Player %c won the game --\n", player);
 				run = 0;
@@ -36,7 +39,41 @@ char game(char player){
 	return '*';
 }
 
+char game_vs_ai(char player){
+	srand(time(NULL));
+	char ai;
+	char grid[6][7];
+	createGrid(grid);
+	if(player == '*')
+		ai = 'o';
+	else
+		ai = '*';
+
+	char run = 1;
+	int move;
+	while(run){
+		printf("\x1b[H\x1b[2J");
+		showGrid(grid);
+
+		printf("\nYour turn: ", player);
+		scanf("%d", &move);
+
+		if(is_move_possible(grid, move - 1)){
+			drop_disk(grid, move - 1, player);
+
+			if(alignment(grid, player) >= 4){
+				printf("\x1b[H\x1b[2J");
+				showGrid(grid);
+				printf(" -- Player %c won the game --\n", player);
+				run = 0;
+			}
+		}
+
+		drop_disk(grid, advanced_advice(grid, ai), ai);
+	}
+}
+
 int main(void){
-	game('*');
+	game_vs_ai('*');
 	return 0;
 }
