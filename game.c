bb1a083621a8998dade8a365d59683632bfb1c50
dumbyplug <iwasdumbyforu@gmail.com>
2025-03-19 18:06:23 +0000

    Update game.c

--- game.c
+++ game.c
@@ -13,43 +13,6 @@ int is_grid_full(char grid[6][7]) {
     return 1; // Grid is full
 }
 
-char game(char player){
-	char initialSymbol = player;
-	char grid[6][7];
-	createGrid(grid);
-
-	char run = 1;
-	int move;
-	while(run){
-		printf("\x1b[H\x1b[2J");
-		showGrid(grid);
-
-		printf("You can play on %d\n", advanced_advice(grid, player) + 1);
-		printf("\nPlayer %c's move: ", player);
-		scanf("%d", &move);
-
-		if(is_move_possible(grid, move - 1)){
-			drop_disk(grid, move - 1, player);
-
-			if(alignment(grid, player) >= 4){
-				printf("\x1b[H\x1b[2J");
-				showGrid(grid);
-				printf(" -- Player %c won the game --\n", player);
-				run = 0;
-			}
-			if(player == '*')
-				player = 'o';
-			else
-				player = '*';
-		}
-
-	}
-	if(initialSymbol == '*')
-		return 'o';
-	return '*';
-}
-
-
 char game_vs_ai(char player) {
     char ai;
     if (player == '*') {
