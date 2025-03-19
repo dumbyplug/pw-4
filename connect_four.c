0b15d126050fda4f941c36b187933631edc7e259
Zaur Valiyev <z.valiyev@ufaz.az>
2025-03-19 15:12:09 +0400

    ISDE ALLAH ICIN

--- connect_four.c
+++ connect_four.c
@@ -6,7 +6,7 @@
 #include "move_possible.c"
 
 int rnd_advice(char grid[6][7]){
-	int min = 0,max = 0;
+	int min = 0,max = 0,index = 0;
 	for (int i = 0; i < 6;i++) {
 		if (is_move_possible(grid,i)){
 			max +=1;
@@ -15,13 +15,12 @@ int rnd_advice(char grid[6][7]){
 	int arr[max];
 	for (int move = 0; move < 6;move++) {
 		if (is_move_possible(grid,move)){
-			arr[min] = move;
-			printf("array: %d\n",arr[min]);
-			min += 1;
+			arr[index] = move;
+			printf("array: %d\n",arr[index]);
+			index += 1;
 		}
 	}
         // Find the random number in the range [min, max]
-	min = 0;
         int rd_num = rand() % (max - min + 1) + min;
 	printf("%d\n",rd_num);
 	printf("%d\n",max);
@@ -32,12 +31,12 @@ int rnd_advice(char grid[6][7]){
 
 int main(void){
 	char grid[6][7]={
+        {'*', '*', '*', '*', '*', '*', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'*', '*', ' ', ' ', '*', ' ', ' '},
+        {'*', '*', '*', '*', '*', '*', ' '},
     };
 
 	rnd_advice(grid);
