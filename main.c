2bcf1149f1f3bfbba66ab374570b904b6fec1242
Zaur Valiyev <z.valiyev@ufaz.az>
2025-03-19 21:52:31 +0400

    belke isliyir belke

--- main.c
+++ main.c
@@ -1,85 +1,34 @@
+#include "alignment.c"
+#include "drop_disk.c"
+#include "move_possible.c"
 #include "create_show.c"
-#include "random_advice.c"
-#include <time.h>
 
-char game(char player){
-	srand(time(NULL));
-	char initialSymbol = player;
+#include <stdlib.h>
+
+void game(char player){
 	char grid[6][7];
 	createGrid(grid);
+
 
 	char run = 1;
 	int move;
 	while(run){
-		printf("\x1b[H\x1b[2J");
 		showGrid(grid);
 
-		printf("You can play on %d\n", advanced_advice(grid, player) + 1);
 		printf("\nPlayer %c's move: ", player);
 		scanf("%d", &move);
 
-		if(is_move_possible(grid, move - 1)){
-			drop_disk(grid, move - 1, player);
-
-			if(alignment(grid, player) >= 4){
-				printf("\x1b[H\x1b[2J");
-				showGrid(grid);
-				printf(" -- Player %c won the game --\n", player);
-				run = 0;
-			}
+		if(is_move_possible(grid, move)){
+			drop_disk(grid, move, player);
 			if(player == '*')
 				player = 'o';
 			else
 				player = '*';
 		}
-
-	}
-	if(initialSymbol == '*')
-		return 'o';
-	return '*';
-}
-
-char game_vs_ai(char player){
-	srand(time(NULL));
-	char ai;
-	char grid[6][7];
-	createGrid(grid);
-	if(player == '*')
-		ai = 'o';
-	else
-		ai = '*';
-
-	char run = 1;
-	int move;
-	while(run){
-		printf("\x1b[H\x1b[2J");
-		showGrid(grid);
-
-		printf("\nYour turn: ", player);
-		scanf("%d", &move);
-
-		if(is_move_possible(grid, move - 1)){
-			drop_disk(grid, move - 1, player);
-
-			if(alignment(grid, player) >= 4){
-				printf("\x1b[H\x1b[2J");
-				showGrid(grid);
-				printf(" -- You won the game --\n");
-				run = 0;
-			}
-		}
-
-		drop_disk(grid, advanced_advice(grid, ai), ai);
-		if(alignment(grid, ai) >= 4){
-			printf("\x1b[H\x1b[2J");
-			showGrid(grid);
-			printf(" -- Ai won the game --\n");
-			run = 0;
-		}
 	}
 }
 
 int main(void){
-	game_vs_ai('*');
+	game('*');
 	return 0;
 }
