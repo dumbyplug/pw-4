a0064e28b9bc59e108b9b03012c52bc3c66ddea2
Ilaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2025-03-19 21:29:14 +0400

    Update random_advice.c

--- random_advice.c
+++ random_advice.c
@@ -5,6 +5,13 @@
 #include "drop_disk.c"
 
 int random_advice(char grid[6][7]){
+	/*
+	This function gives user a random advice based on the move_possible functin, if the move is possible on that random given place it returns that column, if it's not it gives another random advice
+	Input:
+		grid[6][7]: a 6x7 matrix with characters
+	Output:
+		integer: randomly assigned advice
+	*/
 	int random_number = rand() % 7 + 1;
 	int index = 0;
 	while(1){
@@ -19,14 +26,25 @@ int random_advice(char grid[6][7]){
 }
 
 int advanced_advice(char grid[6][7], char player){
+	/*
+	This function gives advice to the users or to the bot, based on alignment function. We check each possible column and using alignment function we assign the column with longest possible alignment to a variable and return that column
+	Input:
+		grid[6][7]: a 6x7 matrix
+		player: character
+	Output:
+		advancely recommended column returns best place to drop the disk
+	*/
 	int longestForPlayer[7], longestForOpponent[7];
 	char opponent;
 	int row;
+
+	//we make sure of player an opponent's disk color
 	if(player == '*')
 		opponent = 'o';
 	else 
 		opponent = '*';
 
+	//this loop determines move possibilities
 	for(int i = 0; i < 7; i++){
 		if(is_move_possible(grid, i)){
 			row = drop_disk(grid, i, player);
@@ -41,6 +59,7 @@ int advanced_advice(char grid[6][7], char player){
 		}
 	}
 
+	//this loop determines max alignment for both players
 	int maxPlayer = 0, maxOpponent = 0;
 	for(int i = 0; i < 7; i++){
 		if(longestForPlayer[i] > maxPlayer){
@@ -51,9 +70,11 @@ int advanced_advice(char grid[6][7], char player){
 		}
 	}
 
+	//we generate a random number to make different move choices
 	int random_number = rand() % 7 + 1;
 	int index = 0, reccomended;
 
+	//finding best aligned column
 	while(1){
 		if(longestForPlayer[index % 7] == maxPlayer){
 			random_number--;
@@ -65,9 +86,11 @@ int advanced_advice(char grid[6][7], char player){
 		index++;
 	}
 
+	//if there's winning move we recommend that one
 	if(maxPlayer >= 4){
 		return reccomended;
 	}
+	//if player can't win they either block opponent's winning move or they play best choice 
 	else if(maxOpponent >= 4){
 		for(int i = 0; i < 7; i++){
 			if(longestForOpponent[i] == maxOpponent){
