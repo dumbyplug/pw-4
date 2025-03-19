7eb7721e4d771d254dc0edcc76e7428c42e71273
Ilaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2025-03-19 21:47:45 +0400

    Update code_with_every_function_so_far.c

--- code_with_every_function_so_far.c
+++ code_with_every_function_so_far.c
@@ -398,15 +398,56 @@ void game_vs_ai(char player){
 	}
 }
 
-int main(void){
-	//game starts with player (which is *)
-	int answer;
-	printf("Do you want to play agains a human or a bot? Choose 1 or 2:\n1.human\n2.bot\n");
-	scanf("%d", &answer);
-	if (answer == 2){
-		game_vs_ai('*');
-	}else{
-		game('*');
+int main() {
+	char run = 1;
+	int choice = 3;
+	char player_char = '*';
+
+	while(run){
+		printf("+------ Connect 4 ------+\n"
+			   "|      (1) Start        |\n"
+			   "|      (2) About        |\n"
+			   "|      (0) Exit         |\n"
+			   "+-----------------------+\n"
+			   "Choose option: ");
+		scanf("%d", &choice);
+		switch(choice){
+			case 1:
+				printf(
+			   "+----------------------------+\n"
+			   "|   (1) Player vs Player     |\n"
+			   "|   (2) Player vs Computer   |\n"
+			   "|   (0) Back                 |\n"
+			   "+----------------------------+\n"
+			   "Choose option: ");
+				scanf("%d", &choice);
+				switch(choice){
+					case 1:
+						player_char = game(player_char);
+						break;
+					case 2:
+						game_vs_ai(player_char);
+						break;
+					case 0:
+						break;
+					default:
+						printf("\n =>> Error. Undefined option <<==\n");
+				}
+				break;
+			case 2:
+				printf("\nThis program is created by\n"
+				"Ayhan Safarov, Tural Haji-Nabili, Ilaha Algayeva and Zaur Valiyev\n\n"
+				"While making of this project, even though caused problems, GitHub was used\n"
+				"Program was made to work in both Windows and Linux terminals\n"
+				"Please support us on our patreon...\n\n");
+				break;
+			case 0:
+				run = 0;
+				break;
+			default:
+				printf("\n =>> Error. Undefined option <<==\n");
+		}
 	}
+
 	return 0;
 }
