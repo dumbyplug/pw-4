90d95fcddfe4a05e7bfc9101d4274259089950af
dumbyplug <iwasdumbyforu@gmail.com>
2025-03-19 11:36:00 +0000

    Update main.c

--- main.c
+++ main.c
@@ -9,22 +9,31 @@ void game(char player){
 	char grid[6][7];
 	createGrid(grid);
 
-
 	char run = 1;
 	int move;
 	while(run){
+		printf("\x1b[H");
 		showGrid(grid);
 
 		printf("\nPlayer %c's move: ", player);
 		scanf("%d", &move);
 
-		if(is_move_possible(grid, move)){
-			drop_disk(grid, move, player);
+		if(is_move_possible(grid, move - 1)){
+			drop_disk(grid, move - 1, player);
+
+			if(alignment(grid) % 10 == 4){
+				printf("\n==============\n"
+						"The player %c won\n", (char)(alignment(grid) / 10));
+				run = 0;
+				showGrid(grid);
+			}
+
 			if(player == '*')
 				player = 'o';
 			else
 				player = '*';
 		}
+
 	}
 }
 
